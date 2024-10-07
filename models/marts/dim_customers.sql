a4ffd24b67aa57b7c9ff28d56ba6272ff63888bf
Isak Kazazic <isak.kazazic@dbtlabscoalesce.com>
2024-10-07 18:47:05 +0000

    sad

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -5,14 +5,7 @@ with customers as (
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status,
-        _etl_loaded_at
-
-    from {{ source('jaffle_shop', 'orders') }}
+    {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
